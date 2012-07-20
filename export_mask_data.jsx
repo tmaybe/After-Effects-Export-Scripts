772f2dc2286b0a6b01f93eedf53d8fe38e8ad244
Thomas Apodaca <tma@smallpower.org>
2012-07-20 00:30:06 -0700

    exporting in plist format; can handle multiple masks on a single layer

--- export_mask_data.jsx
+++ export_mask_data.jsx
@@ -1,27 +1,50 @@
 
 function getAndWriteData(comp, layer)
 {
-	var fnum = 0;
-	var file = new File("test.txt");
-	if (file.open("w"))
+	// how many masks?
+	var mnum = layer.Masks.numProperties;
+	
+	// step through the masks and generate a file with points for each one
+	for (var mc = 0; mc < mnum; mc++)
 	{
-	    for (var t = 0; t < comp.duration; t += comp.frameDuration)
-	    {
-			// get the vertices as an array
-			var mymask = layer.Masks.property(1).property(1).valueAtTime(t, false);
-			var vertices = mymask.vertices;
-			// write the vertices to a line of the file
-			var writeme = "frame:" + fnum + " vertices: [";
-			for (var v = 0; v < vertices.length; v++)
-			{
-				writeme += "[" + vertices[v][0] + "," + vertices[v][1] + "]";
-			}
-			writeme += "]";
+		var fnum = 0;
+		var file = new File("outlines_" + mc + ".plist");
+		if (file.open("w"))
+		{
+			file.writeln('<?xml version="1.0" encoding="UTF-8"?>');
+			file.writeln('<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">');
+			file.writeln('<plist version="1.0">');
+			file.writeln('<array>');
+		    for (var t = 0; t < comp.duration; t += comp.frameDuration)
+		    {
+				// get the vertices as an array (mask numbering starts at 1, that's why I'm adding 1 to mc)
+				var mymask = layer.Masks.property(mc + 1).property(1).valueAtTime(t, false);
+				var vertices = mymask.vertices;
+				// write the vertices to a line of the file
+				var writeme = "";
+				var numv = vertices.length;
+				for (var v = 0; v < numv; v++)
+				{
+					writeme += "				<string>{" + vertices[v][0] + ", " + vertices[v][1] + "}</string>";
+					if (v < numv - 1) { writeme += "\n"; }
+				}
 			
-			file.writeln(writeme);
-			fnum++;
-	    }
-		file.close();
+				file.writeln('	<dict>');
+				file.writeln('		<key>frame</key>');
+				file.writeln('		<real>' + fnum + '</real>');
+				file.writeln('		<key>shapes</key>');
+				file.writeln('		<array>');
+				file.writeln('			<array>');
+				file.writeln(writeme);
+				file.writeln('			</array>');
+				file.writeln('		</array>');
+				file.writeln('	</dict>');			
+				fnum++;
+		    }
+			file.writeln('</array>');
+			file.writeln('</plist>');
+			file.close();
+		}
 	}
 }
 
